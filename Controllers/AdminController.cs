ba0fd1a7f45720d30a1940bd24dbd150bd757d89
jgrissom <jgrissom91@gmail.com>
2019-02-03 12:43:35 -0600

    Admin Controller
    1) Create User Manager which will be used to query the Identity database
    2) Create Index controller method to return the collection of users from the DB to the view

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using EFCoreNewDatabase.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCoreNewDatabase.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public ViewResult Index() => View(userManager.Users);
+    }
+}
