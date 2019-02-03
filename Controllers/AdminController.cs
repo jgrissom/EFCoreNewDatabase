8b7da1a4613ad34d8ff589feb410282933335018
jgrissom <jgrissom91@gmail.com>
2019-02-03 13:22:42 -0600

    Create the controller method that responds to post requests for Admin/Create

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using EFCoreNewDatabase.Models;
+using System.Threading.Tasks;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,5 +19,31 @@ namespace EFCoreNewDatabase.Controllers
 
         public ViewResult Index() => View(userManager.Users);
         public ViewResult Create() => View();
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
     }
 }
