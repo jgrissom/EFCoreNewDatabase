5aae1043dec619b0e5226eb5ae5b2f2f342114f5
jgrissom <jgrissom91@gmail.com>
2019-02-06 13:37:57 -0600

    Account/Login
    1) Create the Account Controller
    2) Add the Login controller method that responds to http get requests (it will display a view)
    3) Create the related view

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreNewDatabase.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            // return url remembers the user's original request
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+    }
+}
