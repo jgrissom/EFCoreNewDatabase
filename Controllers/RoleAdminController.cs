ded7c51d955c9550c53a44f79226dc18d5dbe7da
jgrissom <jgrissom91@gmail.com>
2019-02-06 15:57:58 -0600

    Create Roles
    1) Add RoleAdmin/Create controller method to handle http get requests
    2) Add RoleAdmin/Create view - all that is needed to create the role is a name
    3) Add RoleAdmin/Create controller method to handle http post requests

--- Controllers/RoleAdminController.cs
+++ Controllers/RoleAdminController.cs
@@ -14,6 +14,32 @@ namespace EFCoreNewDatabase.Controllers
         }
 
         public IActionResult Index() => View(roleManager.Roles);
+        public IActionResult Create() => View();
 
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(name);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
 }
