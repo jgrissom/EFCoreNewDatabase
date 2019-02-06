cc871d69abaa621c026e4b9a87cb74f457409e0f
jgrissom <jgrissom91@gmail.com>
2019-02-06 13:43:12 -0600

    Add the controller method to handle http post requests for Account/Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,18 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using EFCoreNewDatabase.Models;
 
 namespace EFCoreNewDatabase.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signInMgr)
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+        }
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
             // return url remembers the user's original request
             ViewBag.returnUrl = returnUrl;
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user of password");
+            }
+            return View(details);
         }
     }
 }
